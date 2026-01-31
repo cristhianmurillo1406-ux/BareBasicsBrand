a7c090b1d25368573b3dd53ae9ea3e9b1f015825
Cristhian <cristhianmurillo1406@gmail.com>
2026-01-31 12:28:55 -0500

    PNG y JPG

--- js/gallery-item.js
+++ js/gallery-item.js
@@ -5,18 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
       title: "WINTER SEASON ’25",
       date: "ENERO 30, 2025",
       images: [
-        "img/gallery/Winter_Season/1.png",
-        "img/gallery/Winter_Season/2.png",
-        "img/gallery/Winter_Season/3.png",
-        "img/gallery/Winter_Season/4.jpg",
-        "img/gallery/Winter_Season/5.png",
-        "img/gallery/Winter_Season/6.jpg",
-        "img/gallery/Winter_Season/7.png",
-        "img/gallery/Winter_Season/8.jpg",
-        "img/gallery/Winter_Season/9.png",
-        "img/gallery/Winter_Season/10.png",
-        "img/gallery/Winter_Season/11.png",
-        "img/gallery/Winter_Season/12.png"
+        "img/gallery/Winter_Season/1.PNG",
+        "img/gallery/Winter_Season/2.PNG",
+        "img/gallery/Winter_Season/3.PNG",
+        "img/gallery/Winter_Season/4.JPG",
+        "img/gallery/Winter_Season/5.PNG",
+        "img/gallery/Winter_Season/6.JPG",
+        "img/gallery/Winter_Season/7.PNG",
+        "img/gallery/Winter_Season/8.JPG",
+        "img/gallery/Winter_Season/9.PNG",
+        "img/gallery/Winter_Season/10.PNG",
+        "img/gallery/Winter_Season/11.PNG",
+        "img/gallery/Winter_Season/12.PNG"
       ]
     },
 
@@ -24,18 +24,18 @@ document.addEventListener("DOMContentLoaded", () => {
       title: "STREET ART",
       date: "ABRIL 11, 2025",
       images: [
-        "img/gallery/Street_Art/1.jpg",
-        "img/gallery/Street_Art/2.jpg",
-        "img/gallery/Street_Art/3.jpg",
-        "img/gallery/Street_Art/4.jpg",
-        "img/gallery/Street_Art/5.jpg",
-        "img/gallery/Street_Art/6.jpg",
-        "img/gallery/Street_Art/7.jpg",
-        "img/gallery/Street_Art/8.jpg",
-        "img/gallery/Street_Art/9.jpg",
-        "img/gallery/Street_Art/10.jpg",
-        "img/gallery/Street_Art/11.jpg",
-        "img/gallery/Street_Art/12.jpg"
+        "img/gallery/Street_Art/1.JPG",
+        "img/gallery/Street_Art/2.JPG",
+        "img/gallery/Street_Art/3.JPG",
+        "img/gallery/Street_Art/4.JPG",
+        "img/gallery/Street_Art/5.JPG",
+        "img/gallery/Street_Art/6.JPG",
+        "img/gallery/Street_Art/7.JPG",
+        "img/gallery/Street_Art/8.JPG",
+        "img/gallery/Street_Art/9.JPG",
+        "img/gallery/Street_Art/10.JPG",
+        "img/gallery/Street_Art/11.JPG",
+        "img/gallery/Street_Art/12.JPG"
       ]
     }
   };
